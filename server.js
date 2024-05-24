cd46ba18f6df87af1bedc9501d5259cf2db5f6e9
Marilin Pajumägi <marilin.pajumagi@tktk.ee>
2024-05-24 14:26:47 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,86 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+
+    
+    let url = 'https://api.themoviedb.org/3/movie/550988?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
+    axios.get(url)
+    .then(response => {
+        console.log(response.data.title);
+        let data = response.data;
+        let releaseDate = new Date(data.release_date).getFullYear();
+        let genres = '';
+
+        data.genres.forEach(genre => {
+            genres = genres + `${genre.name}, `;
+        });
+
+        
+        let genresUpdated = genres.slice(0, -2) + '.';
+        moviePoster = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+        console.log(genresUpdated);
+        let currentYear = new Date().getFullYear();
+        res.render('index', {movieData: data, releaseDate: releaseDate, genres: genresUpdated, poster: moviePoster, year: currentYear});
+    });
+    
+});
+
+//new route
+app.post('/getmovie', (req, res) => {
+	const movieToSearch =
+		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
+			? req.body.queryResult.parameters.movie
+			: '';
+
+	const reqUrl = encodeURI(
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=cc6f9d1b`
+	);
+	http.get(
+		reqUrl,
+		responseFromAPI => {
+			let completeResponse = ''
+			responseFromAPI.on('data', chunk => {
+				completeResponse += chunk
+			})
+			responseFromAPI.on('end', () => {
+				const movie = JSON.parse(completeResponse);
+                if (!movie || !movie.Title) {
+                    return res.json({
+                        fulfillmentText: 'Sorry, we could not find the movie you are asking for.',
+                        source: 'getmovie'
+                    });
+                }
+
+				let dataToSend = movieToSearch;
+				dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${
+					movie.Director
+				} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.`;
+
+				return res.json({
+					fulfillmentText: dataToSend,
+					source: 'getmovie'
+				});
+			})
+		},
+		error => {
+			return res.json({
+				fulfillmentText: 'Could not get results at this time',
+				source: 'getmovie'
+			});
+		}
+	)
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running');
+});
